890fb4750d554287a5360dbf496d2579aa9c5ac4
soulmate1139 <92954297+soulmate1139@users.noreply.github.com>
2024-08-31 13:25:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ if (window.location.href.includes("?h5") == false && visitedH5 !== "true") {
     window.location = "../notfound.html";
 } else {
     localStorage.setItem('visitedH5', true);
+    if (window.location.href.includes("?h5") == true) {
+        window.location = window.location.pathname;
+    }
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
     if (/android/i.test(userAgent) || (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)) {
         window.location = "mobile/index.html"
